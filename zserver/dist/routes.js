e8d3f69eaa9a5b254652235915ccfa7d90ca6bbb
Dharambir Agrawal <dev.dharambir@gmail.com>
2025-01-10 05:15:03 +0000

    Added the new servers

---
+++ zserver/dist/routes.js
@@ -0,0 +1,10 @@
+"use strict";
+// // import blogRoutes from './api/blog/blogRoutes';
+// // import courseRoutes from './api/courses/courseRoutes';
+// // import productRoutes from './api/products/productRoutes';
+// const routes = (app) => {
+//   app.use('/api/blog', blogRoutes);
+//   app.use('/api/courses', courseRoutes);
+//   app.use('/api/products', productRoutes);
+// };
+// export default routes;
