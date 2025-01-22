aeb374c4b227eea36c970b6630f6d3d91ccb9687
Dharambir Agrawal <dev.dharambir@gmail.com>
2025-01-22 19:35:22 +0000

    Middleware updated

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,32 +1,50 @@
-import { NextResponse } from 'next/server';
-import { parse } from 'cookie';
-import { getCookie } from './lib/cookies';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { parse } from "cookie";
+import { getCookie } from "./lib/cookies";
 
-export async function middleware(req) {
+export async function middleware(request: NextRequest) {
   // Get the cookies from the request
-  const accessToken= await getCookie('access_token');
-    const refreshToken = await  getCookie('refresh_token');  
-    // console.log('Access Token:', accessToken);
-    // console.log('Refresh Token:', refreshToken);
-    const cookies = parse(req.headers.get('cookie') || '');
-console.log(cookies)
-
+  const accessToken = await getCookie("access_token");
+  const refreshToken = await getCookie("refresh_token");
+  // console.log('Access Token:', accessToken);
+  // console.log('Refresh Token:', refreshToken);
+  const cookies = parse(request.headers.get("cookie") || "");
+  console.log(cookies);
+  console.log(accessToken);
+  console.log(refreshToken);
+  console.log("........................");
+  let cookie = request.cookies.get("access_token");
+  console.log(cookie); // => { name: 'nextjs', value: 'fast', Path: '/' }
+  const allCookies = request.cookies.getAll();
+  console.log(allCookies); // => [{ name: 'nextjs', value: 'fast' }]
 
   // Get the sessionId from the Authorization header (if available)
-  const authHeader = req.headers.get('Authorization');
+  const authHeader = request.headers.get("Authorization");
   let sessionId = null;
-  if (authHeader && authHeader.startsWith('Bearer ')) {
+  if (authHeader && authHeader.startsWith("Bearer ")) {
     sessionId = authHeader.substring(7); // Remove "Bearer " prefix
   }
-  console.log('authHeader:', authHeader)
-  console.log('sessionId:', sessionId);
+  console.log("authHeader:", authHeader);
+  console.log("sessionId:", sessionId);
 
-//   console.log('Access Token:', accessToken);
-//   console.log('Refresh Token:', refreshToken);
-//   console.log('Session ID:', sessionId);
+  //   console.log('Access Token:', accessToken);
+  //   console.log('Refresh Token:', refreshToken);
+  //   console.log('Session ID:', sessionId);
+
+const res= await fetch(`${process.env.SERVER_BASE_URL}/api/test/server`);
+const header= res.headers.get("Authorization");
+console.log(header)
+// console.log(res)
+
+
 
   // Optionally, you can handle authentication or other logic here
 
   // Allow the request to continue
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: "/auth/:path*",
+};
