5879bce178a639303209a0a7795f960bb5e414de
Dharambir Agrawal <dev.dharambir@gmail.com>
2025-01-23 21:20:16 +0000

    Working on middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,13 +1,12 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { cookies } from 'next/headers'
 export async function middleware(request: NextRequest) {
-  const cookieStore = await cookies()
   // Get the sessionId cookie
-  const cookie = await cookieStore.getAll()
-console.log(cookie)
+
+
+// console.log(cookie)
 const allCookies = request.cookies.getAll()
-console.log(allCookies) // => [{ name: 'nextjs', value: 'fast' }]
+// console.log(allCookies) // => [{ name: 'nextjs', value: 'fast' }]
   return NextResponse.next();
 }
 
