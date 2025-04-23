ad323ec9498b14899555f4f067f527f2e10f719d
Dharambir Agrawal <dev.dharambir@gmail.com>
2025-04-22 21:57:53 -0500

    Updated the user tables theme

--- client/src/app/(dashboard)/components/users/UserTable.tsx
+++ client/src/app/(dashboard)/components/users/UserTable.tsx
@@ -1,22 +1,180 @@
+// "use client";
+
+// import { UserType } from "./types";
+// import Image from "next/image";
+// interface ActionRequiredProps {
+//   action: true;
+//   // handleEdit: (body: Record<string, any>) => void;
+//   handleEdit: (user: UserType) => void;
+//   handleDelete: (userId: string) => void;
+// }
+
+// // Define a type for when action is false (optional handlers)
+// interface NoActionRequiredProps {
+//   action: false;
+//   handleEdit?: () => void;
+//   handleDelete?: () => void;
+// }
+
+// // Combine the two using a conditional type
+// type UserTableProps = (ActionRequiredProps | NoActionRequiredProps) & {
+//   currentBody: UserType[];
+// };
+
+// const UsersTable: React.FC<UserTableProps> = ({
+//   currentBody,
+//   action,
+//   handleEdit,
+//   handleDelete,
+// }) => {
+//   const data = {
+//     header: [
+//       "Image",
+//       "Name",
+//       "Email",
+//       "Role",
+//       "Status",
+//       "Email Verified",
+//       "Actions",
+//     ],
+//   };
+
+//   return (
+//     // <div className="mx-auto p-4 bg-gray-50 rounded-lg shadow-lg">
+
+//     <div className="overflow-x-auto bg-white rounded-lg shadow">
+//       <table className="min-w-full">
+//         <thead>
+//           <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+//             {data.header.map((header, index) => (
+//               <th className="py-3 px-6 text-left" key={index}>
+//                 {header}
+//               </th>
+//             ))}
+//           </tr>
+//         </thead>
+//         <tbody className="text-gray-600 text-sm font-light">
+//           {currentBody.map((user) => (
+//             <tr
+//               key={user.userId}
+//               className="border-b border-gray-200 hover:bg-gray-100"
+//             >
+//               <td className="py-3 px-6 text-left whitespace-nowrap">
+//                 <Image
+//                   src={user.providerProfileImage || "/placeholder.svg"}
+//                   alt={`${user.firstName} ${user.lastName}`}
+//                   className="w-10 h-10 rounded-full"
+//                   width={40}
+//                   height={40}
+//                 />
+//               </td>
+//               <td className="py-3 px-6 text-left">{`${user.firstName} ${user.lastName}`}</td>
+//               <td className="py-3 px-6 text-left">{user.email}</td>
+//               <td className="py-3 px-6 text-center">
+//                 <span
+//                   className={`px-3 py-1 rounded-full text-xs ${
+//                     user.role === "ADMIN"
+//                       ? "bg-purple-200 text-purple-800"
+//                       : "bg-yellow-200 text-yellow-800"
+//                   }`}
+//                 >
+//                   {user.role}
+//                 </span>
+//               </td>
+//               <td className="py-3 px-6 text-center">
+//                 <span
+//                   className={`px-3 py-1 rounded-full text-xs ${
+//                     user.accountStatus === "ACTIVE"
+//                       ? "bg-green-200 text-green-800"
+//                       : "bg-red-200 text-red-800"
+//                   }`}
+//                 >
+//                   {user.accountStatus}
+//                 </span>
+//               </td>
+//               <td className="py-3 px-6 text-center">
+//                 <span
+//                   className={`px-3 py-1 rounded-full text-xs ${
+//                     user.accountStatus
+//                       ? "bg-green-200 text-green-800"
+//                       : "bg-red-200 text-red-800"
+//                   }`}
+//                 >
+//                   {user.isEmailVerified ? "Yes" : "No"}
+//                 </span>
+//               </td>
+//               {action && (
+//                 <td className="py-3 px-6 text-center">
+//                   <div className="flex item-center justify-center">
+//                     <button
+//                       onClick={() => handleEdit(user)}
+//                       className="transform hover:text-blue-500 hover:scale-110 transition duration-300 ease-in-out mr-3"
+//                     >
+//                       <svg
+//                         xmlns="http://www.w3.org/2000/svg"
+//                         className="h-5 w-5"
+//                         fill="none"
+//                         viewBox="0 0 24 24"
+//                         stroke="currentColor"
+//                       >
+//                         <path
+//                           strokeLinecap="round"
+//                           strokeLinejoin="round"
+//                           strokeWidth="2"
+//                           d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+//                         />
+//                       </svg>
+//                     </button>
+//                     <button
+//                       onClick={() => {}}
+//                       className="transform hover:text-red-500 hover:scale-110 transition duration-300 ease-in-out"
+//                     >
+//                       <svg
+//                         xmlns="http://www.w3.org/2000/svg"
+//                         className="h-5 w-5"
+//                         fill="none"
+//                         viewBox="0 0 24 24"
+//                         stroke="currentColor"
+//                       >
+//                         <path
+//                           strokeLinecap="round"
+//                           strokeLinejoin="round"
+//                           strokeWidth="2"
+//                           d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+//                         />
+//                       </svg>
+//                     </button>
+//                   </div>
+//                 </td>
+//               )}
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+
+//     // </div>
+//   );
+// };
+
+// export default UsersTable;
 "use client";
 
 import { UserType } from "./types";
 import Image from "next/image";
+
 interface ActionRequiredProps {
   action: true;
-  // handleEdit: (body: Record<string, any>) => void;
   handleEdit: (user: UserType) => void;
   handleDelete: (userId: string) => void;
 }
 
-// Define a type for when action is false (optional handlers)
 interface NoActionRequiredProps {
   action: false;
   handleEdit?: () => void;
   handleDelete?: () => void;
 }
 
-// Combine the two using a conditional type
 type UserTableProps = (ActionRequiredProps | NoActionRequiredProps) & {
   currentBody: UserType[];
 };
@@ -40,12 +198,10 @@ const UsersTable: React.FC<UserTableProps> = ({
   };
 
   return (
-    // <div className="mx-auto p-4 bg-gray-50 rounded-lg shadow-lg">
-
-    <div className="overflow-x-auto bg-white rounded-lg shadow">
+    <div className="overflow-x-auto bg-white dark:bg-gray-800 rounded-lg shadow transition-colors">
       <table className="min-w-full">
         <thead>
-          <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+          <tr className="bg-gray-200 dark:bg-gray-700 text-gray-600 dark:text-gray-200 uppercase text-sm leading-normal">
             {data.header.map((header, index) => (
               <th className="py-3 px-6 text-left" key={index}>
                 {header}
@@ -53,11 +209,11 @@ const UsersTable: React.FC<UserTableProps> = ({
             ))}
           </tr>
         </thead>
-        <tbody className="text-gray-600 text-sm font-light">
+        <tbody className="text-gray-600 dark:text-gray-300 text-sm font-light">
           {currentBody.map((user) => (
             <tr
               key={user.userId}
-              className="border-b border-gray-200 hover:bg-gray-100"
+              className="border-b border-gray-200 dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors"
             >
               <td className="py-3 px-6 text-left whitespace-nowrap">
                 <Image
@@ -72,10 +228,10 @@ const UsersTable: React.FC<UserTableProps> = ({
               <td className="py-3 px-6 text-left">{user.email}</td>
               <td className="py-3 px-6 text-center">
                 <span
-                  className={`px-3 py-1 rounded-full text-xs ${
+                  className={`px-3 py-1 rounded-full text-xs font-medium ${
                     user.role === "ADMIN"
-                      ? "bg-purple-200 text-purple-800"
-                      : "bg-yellow-200 text-yellow-800"
+                      ? "bg-purple-200 dark:bg-purple-700 text-purple-800 dark:text-purple-100"
+                      : "bg-yellow-200 dark:bg-yellow-700 text-yellow-800 dark:text-yellow-100"
                   }`}
                 >
                   {user.role}
@@ -83,10 +239,10 @@ const UsersTable: React.FC<UserTableProps> = ({
               </td>
               <td className="py-3 px-6 text-center">
                 <span
-                  className={`px-3 py-1 rounded-full text-xs ${
+                  className={`px-3 py-1 rounded-full text-xs font-medium ${
                     user.accountStatus === "ACTIVE"
-                      ? "bg-green-200 text-green-800"
-                      : "bg-red-200 text-red-800"
+                      ? "bg-green-200 dark:bg-green-700 text-green-800 dark:text-green-100"
+                      : "bg-red-200 dark:bg-red-700 text-red-800 dark:text-red-100"
                   }`}
                 >
                   {user.accountStatus}
@@ -94,10 +250,10 @@ const UsersTable: React.FC<UserTableProps> = ({
               </td>
               <td className="py-3 px-6 text-center">
                 <span
-                  className={`px-3 py-1 rounded-full text-xs ${
-                    user.accountStatus
-                      ? "bg-green-200 text-green-800"
-                      : "bg-red-200 text-red-800"
+                  className={`px-3 py-1 rounded-full text-xs font-medium ${
+                    user.isEmailVerified
+                      ? "bg-green-200 dark:bg-green-700 text-green-800 dark:text-green-100"
+                      : "bg-red-200 dark:bg-red-700 text-red-800 dark:text-red-100"
                   }`}
                 >
                   {user.isEmailVerified ? "Yes" : "No"}
@@ -108,7 +264,7 @@ const UsersTable: React.FC<UserTableProps> = ({
                   <div className="flex item-center justify-center">
                     <button
                       onClick={() => handleEdit(user)}
-                      className="transform hover:text-blue-500 hover:scale-110 transition duration-300 ease-in-out mr-3"
+                      className="transform hover:text-blue-500 dark:hover:text-blue-300 hover:scale-110 transition duration-300 ease-in-out mr-3"
                     >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -126,8 +282,8 @@ const UsersTable: React.FC<UserTableProps> = ({
                       </svg>
                     </button>
                     <button
-                      onClick={() => {}}
-                      className="transform hover:text-red-500 hover:scale-110 transition duration-300 ease-in-out"
+                      onClick={() => handleDelete && handleDelete(user.userId)}
+                      className="transform hover:text-red-500 dark:hover:text-red-400 hover:scale-110 transition duration-300 ease-in-out"
                     >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -152,8 +308,6 @@ const UsersTable: React.FC<UserTableProps> = ({
         </tbody>
       </table>
     </div>
-
-    // </div>
   );
 };
 
--- client/src/app/(dashboard)/dashboard/users/page.tsx
+++ client/src/app/(dashboard)/dashboard/users/page.tsx
@@ -1,9 +1,177 @@
+// "use client";
+// import { useState, useEffect } from "react";
+// import UsersTable from "../../components/users/UserTable";
+// import EditUserDialog from "../../components/users/EditUserDialog";
+// import { getUsers } from "./actions";
+// import { UserType } from "../../components/users/types";
+// export default function UsersPage() {
+//   const [users, setUsers] = useState<UserType[]>([]);
+//   const [refresh, setRefresh] = useState(false);
+
+//   const [filteredUsers, setFilteredUsers] = useState<UserType[]>(users);
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const [searchTerm, setSearchTerm] = useState("");
+//   const [editingUser, setEditingUser] = useState<UserType | null>(null);
+//   const [isDialogOpen, setIsDialogOpen] = useState(false);
+//   const [statusFilter, setStatusFilter] = useState<
+//     "all" | "INACTIVE" | "ACTIVE" | "PENDING" | "SUSPENDED"
+//   >("all");
+//   const [roleFilter, setRoleFilter] = useState<
+//     "all" | "ADMIN" | "USER" | "AUTHOR"
+//   >("all");
+
+//   const [filter, setFilter] = useState({
+//     pagination: {
+//       currentPage: 1,
+//       pageLimit: 10,
+//       totalPage: 1,
+//       totalUsers: 1,
+//     },
+//   });
+
+//   useEffect(() => {
+//     let filtered = users.filter((user) =>
+//       Object.values(user).some((value) =>
+//         value.toString().toLowerCase().includes(searchTerm.toLowerCase())
+//       )
+//     );
+
+//     if (statusFilter !== "all") {
+//       filtered = filtered.filter((user) => user.accountStatus === statusFilter);
+//     }
+
+//     if (roleFilter !== "all") {
+//       filtered = filtered.filter((user) => user.role === roleFilter);
+//     }
+//     setFilteredUsers(filtered);
+//     setCurrentPage(1);
+//   }, [searchTerm, users, statusFilter, roleFilter]);
+
+//   useEffect(() => {
+//     const gettingUsers = async () => {
+//       const users = await getUsers();
+//       setUsers(users.data);
+//     };
+//     gettingUsers();
+//   }, [refresh]);
+//   const usersPerPage = 10;
+
+//   const indexOfLastUser = currentPage * usersPerPage;
+//   const indexOfFirstUser = indexOfLastUser - usersPerPage;
+//   const totalPages = Math.ceil(filteredUsers.length / usersPerPage);
+
+//   const handlePageChange = (pageNumber: number) => {
+//     setCurrentPage(pageNumber);
+//   };
+
+//   const handleEdit = (user: UserType) => {
+//     setEditingUser(user);
+//     setIsDialogOpen(true);
+//   };
+
+//   const handleDelete = (userId: string) => {
+//     setUsers(users.filter((user) => user.userId !== userId));
+//   };
+
+//   return (
+//     <div className="md:p-8">
+//       <h1 className="text-3xl font-bold mb-6 text-gray-800">User Management</h1>
+//       {/* <UserTable /> */}
+//       <div className="mx-auto p-4 bg-gray-50 rounded-lg shadow-lg">
+//         <div className="mb-6 flex flex-col md:flex-row md:items-center md:justify-between gap-4">
+//           <input
+//             type="text"
+//             placeholder="Search users..."
+//             className="w-full md:w-1/3 p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+//             value={searchTerm}
+//             onChange={(e) => setSearchTerm(e.target.value)}
+//           />
+//           <div className="flex gap-4">
+//             <select
+//               value={statusFilter}
+//               onChange={(e) =>
+//                 setStatusFilter(
+//                   e.target.value as
+//                     | "all"
+//                     | "INACTIVE"
+//                     | "ACTIVE"
+//                     | "PENDING"
+//                     | "SUSPENDED"
+//                 )
+//               }
+//               className="p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+//             >
+//               <option value="all">All Status</option>
+//               <option value="ACTIVE">Active</option>
+//               <option value="INACTIVE">Inactive</option>
+//               <option value="PENDING">Pending</option>
+//               <option value="SUSPENDED">Suspended</option>
+//             </select>
+//             <select
+//               value={roleFilter}
+//               onChange={(e) =>
+//                 setRoleFilter(
+//                   e.target.value as "all" | "ADMIN" | "USER" | "AUTHOR"
+//                 )
+//               }
+//               className="p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+//             >
+//               <option value="all">All Roles</option>
+//               <option value="ADMIN">Admin</option>
+//               <option value="USER">User</option>
+//               <option value="AUTHOR">Author</option>
+//             </select>
+//           </div>
+//         </div>
+
+//         <UsersTable
+//           currentBody={filteredUsers}
+//           action={true}
+//           handleDelete={handleDelete}
+//           handleEdit={handleEdit}
+//         />
+//         <div className="mt-6 flex flex-col sm:flex-row justify-between items-center">
+//           <div className="mb-4 sm:mb-0 text-sm text-gray-600">
+//             Showing {indexOfFirstUser + 1} to{" "}
+//             {Math.min(indexOfLastUser, filteredUsers.length)} of{" "}
+//             {filteredUsers.length} entries
+//           </div>
+//           <div className="flex flex-wrap justify-center gap-2">
+//             {Array.from({ length: totalPages }, (_, i) => i + 1).map(
+//               (pageNumber) => (
+//                 <button
+//                   key={pageNumber}
+//                   onClick={() => handlePageChange(pageNumber)}
+//                   className={`px-3 py-1 rounded-md text-sm font-medium ${
+//                     currentPage === pageNumber
+//                       ? "bg-blue-500 text-white"
+//                       : "bg-white text-blue-500 hover:bg-blue-100"
+//                   } transition duration-300 ease-in-out`}
+//                 >
+//                   {pageNumber}
+//                 </button>
+//               )
+//             )}
+//           </div>
+//         </div>
+//         {isDialogOpen && editingUser && (
+//           <EditUserDialog
+//             user={editingUser}
+//             onClose={() => setIsDialogOpen(false)}
+//             setRefresh={setRefresh}
+//           />
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
 "use client";
 import { useState, useEffect } from "react";
 import UsersTable from "../../components/users/UserTable";
 import EditUserDialog from "../../components/users/EditUserDialog";
 import { getUsers } from "./actions";
 import { UserType } from "../../components/users/types";
+
 export default function UsersPage() {
   const [users, setUsers] = useState<UserType[]>([]);
   const [refresh, setRefresh] = useState(false);
@@ -19,15 +187,6 @@ export default function UsersPage() {
   const [roleFilter, setRoleFilter] = useState<
     "all" | "ADMIN" | "USER" | "AUTHOR"
   >("all");
-
-  const [filter, setFilter] = useState({
-    pagination: {
-      currentPage: 1,
-      pageLimit: 10,
-      totalPage: 1,
-      totalUsers: 1,
-    },
-  });
 
   useEffect(() => {
     let filtered = users.filter((user) =>
@@ -54,8 +213,8 @@ export default function UsersPage() {
     };
     gettingUsers();
   }, [refresh]);
-  const usersPerPage = 10;
 
+  const usersPerPage = 10;
   const indexOfLastUser = currentPage * usersPerPage;
   const indexOfFirstUser = indexOfLastUser - usersPerPage;
   const totalPages = Math.ceil(filteredUsers.length / usersPerPage);
@@ -74,15 +233,16 @@ export default function UsersPage() {
   };
 
   return (
-    <div className="md:p-8">
-      <h1 className="text-3xl font-bold mb-6 text-gray-800">User Management</h1>
-      {/* <UserTable /> */}
-      <div className="mx-auto p-4 bg-gray-50 rounded-lg shadow-lg">
+    <div className="md:p-8 dark:bg-gray-900 dark:text-white min-h-screen transition-colors">
+      <h1 className="text-3xl font-bold mb-6 text-gray-800 dark:text-white">
+        User Management
+      </h1>
+      <div className="mx-auto p-4 bg-gray-50 dark:bg-gray-800 rounded-lg shadow-lg transition-colors">
         <div className="mb-6 flex flex-col md:flex-row md:items-center md:justify-between gap-4">
           <input
             type="text"
             placeholder="Search users..."
-            className="w-full md:w-1/3 p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full md:w-1/3 p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
@@ -99,7 +259,7 @@ export default function UsersPage() {
                     | "SUSPENDED"
                 )
               }
-              className="p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
             >
               <option value="all">All Status</option>
               <option value="ACTIVE">Active</option>
@@ -114,7 +274,7 @@ export default function UsersPage() {
                   e.target.value as "all" | "ADMIN" | "USER" | "AUTHOR"
                 )
               }
-              className="p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
             >
               <option value="all">All Roles</option>
               <option value="ADMIN">Admin</option>
@@ -131,7 +291,7 @@ export default function UsersPage() {
           handleEdit={handleEdit}
         />
         <div className="mt-6 flex flex-col sm:flex-row justify-between items-center">
-          <div className="mb-4 sm:mb-0 text-sm text-gray-600">
+          <div className="mb-4 sm:mb-0 text-sm text-gray-600 dark:text-gray-300">
             Showing {indexOfFirstUser + 1} to{" "}
             {Math.min(indexOfLastUser, filteredUsers.length)} of{" "}
             {filteredUsers.length} entries
@@ -145,7 +305,7 @@ export default function UsersPage() {
                   className={`px-3 py-1 rounded-md text-sm font-medium ${
                     currentPage === pageNumber
                       ? "bg-blue-500 text-white"
-                      : "bg-white text-blue-500 hover:bg-blue-100"
+                      : "bg-white text-blue-500 hover:bg-blue-100 dark:bg-gray-700 dark:text-blue-300 dark:hover:bg-gray-600"
                   } transition duration-300 ease-in-out`}
                 >
                   {pageNumber}
